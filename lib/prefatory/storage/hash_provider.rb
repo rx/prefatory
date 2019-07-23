118ea3cf661f2c63f8af5f32e454ac22b132f783
Tyler Lemburg <tyler@geotix.com>
2019-07-23 16:20:04 -0400

    Add basic hash provider

---
+++ lib/prefatory/storage/hash_provider.rb
@@ -0,0 +1,36 @@
+module Prefatory
+  module Storage
+    # a simple store, for when prefatory is injected into your interface but
+    # only ephemeral storage is required
+    class HashProvider
+      def initialize(
+          options = nil,
+          ttl = nil,
+          key_generator: Prefatory.config.keys.generator.new
+        )
+        @hash = {}
+        @key_generator = key_generator
+      end
+
+      def set(key, value, ttl=nil)
+        @hash.store(key, value)
+      end
+
+      def get(key)
+        value = @hash.fetch(key){nil}
+      end
+
+      def key?(key)
+        @hash.key?(key)
+      end
+
+      def delete(key)
+        @hash.delete(key)
+      end
+
+      def next_key(obj = nil)
+        @key_generator.key(obj)
+      end
+    end
+  end
+end
